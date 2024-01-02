76504910daac16562265cfa6d896c0e99bf8ab03
murilo <murilohenrique12344@gmail.com>
2024-01-02 16:47:26 -0300

    atualizacoes-agenda

--- contact/urls.py
+++ contact/urls.py
@@ -5,10 +5,14 @@ from django.conf.urls.static import static
 
 app_name = 'contact'
 
-
-
 urlpatterns = [
+    path('search/', views.search, name='search'),
     path("", views.index, name='index'),
+
+    #contact (CRUD)
+        path('contact/<int:contact_id>/', views.contact, name='contact'),
+        path('contact/create/', views.create, name='contact'),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
