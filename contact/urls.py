7f4880f3db515843e15e93668b1ad599fc320377
murilo <murilohenrique12344@gmail.com>
2024-01-02 09:06:45 -0300

    atualizacao-models

--- contact/urls.py
+++ contact/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from contact import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'contact'
 
@@ -8,3 +10,6 @@ app_name = 'contact'
 urlpatterns = [
     path("", views.index, name='index'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
